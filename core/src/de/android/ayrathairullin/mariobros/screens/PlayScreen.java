bb0e1cd8b2a3a1713f75911ae98547038bd04385
Ayrat <ayrat1@mail.ru>
2018-02-16 22:40:05 +0300

    v3 complete

--- core/src/de/android/ayrathairullin/mariobros/screens/PlayScreen.java
+++ core/src/de/android/ayrathairullin/mariobros/screens/PlayScreen.java
@@ -4,17 +4,24 @@ package de.android.ayrathairullin.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import de.android.ayrathairullin.mariobros.MarioBros;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gameCam);
     }
 
     @Override
@@ -26,6 +33,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -33,7 +41,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
