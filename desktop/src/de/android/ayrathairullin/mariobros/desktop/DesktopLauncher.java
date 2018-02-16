ac82583706953d4dab0acd606a62b7b09c24686d
Ayrat <ayrat1@mail.ru>
2018-02-16 10:02:22 +0300

    Initial commit

---
+++ desktop/src/de/android/ayrathairullin/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.android.ayrathairullin.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.android.ayrathairullin.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
