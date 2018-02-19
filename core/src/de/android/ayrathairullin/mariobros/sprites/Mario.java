098b1551ed154064327eed1ef7a99216059a219b
Ayrat <ayrat1@mail.ru>
2018-02-19 22:26:53 +0300

    v11 complete

--- core/src/de/android/ayrathairullin/mariobros/sprites/Mario.java
+++ core/src/de/android/ayrathairullin/mariobros/sprites/Mario.java
@@ -1,6 +1,7 @@
 package de.android.ayrathairullin.mariobros.sprites;
 
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -8,26 +9,93 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 
 import de.android.ayrathairullin.mariobros.MarioBros;
 import de.android.ayrathairullin.mariobros.screens.PlayScreen;
 
-public class Mario extends Sprite{
+public class Mario extends Sprite {
+    public enum State {
+        FALLING,
+        JUMPING,
+        STANDING,
+        RUNNING
+    }
+
+    public State currentState, previousState;
     public World world;
     public Body b2body;
     private TextureRegion marioStand;
+    private Animation<TextureRegion> marioRun, marioJump;
+    private float stateTimer;
+    private boolean runningRight;
 
     public Mario(World world, PlayScreen screen) {
         super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
-        defineMario();
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for (int i = 1; i < 4; i++)
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+        marioRun = new Animation(.1f, frames);
+        frames.clear();
+        for (int i = 4; i < 6; i++)
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+        marioJump = new Animation(.1f, frames);
+//        frames.clear();
         marioStand = new TextureRegion(getTexture(), 0, 0, 16, 16);
+        defineMario();
         setBounds(0, 0, 16 / MarioBros.PPM, 16 / MarioBros.PPM);
         setRegion(marioStand);
     }
 
     public void update(float dt) {
         setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(dt));
+    }
+
+    public TextureRegion getFrame(float dt) {
+        currentState = getState();
+        TextureRegion region;
+        switch (currentState) {
+            case JUMPING:
+                region = marioJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = marioRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+            default:
+                region = marioStand;
+                break;
+        }
+        if ((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = false;
+        }else if ((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()){
+            region.flip(true, false);
+            runningRight = true;
+        }
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+        return region;
+    }
+
+    public State getState() {
+        if (b2body.getLinearVelocity().y > 0 || (b2body.getLinearVelocity().y < 0 && previousState == State.JUMPING)) {
+            return State.JUMPING;
+        } else if (b2body.getLinearVelocity().y < 0) {
+            return State.FALLING;
+        } else if (b2body.getLinearVelocity().x != 0) {
+            return State.RUNNING;
+        } else {
+            return State.STANDING;
+        }
+
     }
 
     public void defineMario() {
