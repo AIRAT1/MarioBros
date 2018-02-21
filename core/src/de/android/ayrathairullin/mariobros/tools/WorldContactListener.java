96edd9fa120506283d5d92ce641b3e37115e34d3
Ayrat <ayrat1@mail.ru>
2018-02-21 14:28:59 +0300

    v17 complete

--- core/src/de/android/ayrathairullin/mariobros/tools/WorldContactListener.java
+++ core/src/de/android/ayrathairullin/mariobros/tools/WorldContactListener.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
+import de.android.ayrathairullin.mariobros.MarioBros;
+import de.android.ayrathairullin.mariobros.sprites.Enemy;
 import de.android.ayrathairullin.mariobros.sprites.InteractiveTileObject;
 
 public class WorldContactListener implements ContactListener{
@@ -14,12 +16,21 @@ public class WorldContactListener implements ContactListener{
     public void beginContact(Contact contact) {
         Fixture fixA = contact.getFixtureA();
         Fixture fixB = contact.getFixtureB();
+        int cDef = fixA.getFilterData().categoryBits | fixB.getFilterData().categoryBits;
         if (fixA.getUserData() == "head" || fixB.getUserData() == "head") {
             Fixture head = fixA.getUserData() == "head" ? fixA : fixB;
             Fixture object = head == fixA ? fixB : fixA;
             if (object.getUserData() != null && InteractiveTileObject.class.isAssignableFrom(object.getUserData().getClass())) {
                 ((InteractiveTileObject)object.getUserData()).onHeadHit();
             }
+        }
+        switch (cDef) {
+            case MarioBros.ENEMY_HEAD_BIT | MarioBros.MARIO_BIT:
+                if (fixA.getFilterData().categoryBits == MarioBros.ENEMY_HEAD_BIT) {
+                    ((Enemy)fixA.getUserData()).hitOnHead();
+                }else if (fixB.getFilterData().categoryBits == MarioBros.ENEMY_HEAD_BIT) {
+                    ((Enemy)fixB.getUserData()).hitOnHead();
+                }
         }
     }
 
