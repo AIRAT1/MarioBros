e4c84b597647fe860a1ab860dcb615ee248efda8
Ayrat <ayrat1@mail.ru>
2018-02-24 23:57:02 +0300

    v29 05.00

---
+++ core/src/de/android/ayrathairullin/mariobros/sprites/enemies/Turtle.java
@@ -0,0 +1,80 @@
+package de.android.ayrathairullin.mariobros.sprites.enemies;
+
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.utils.Array;
+
+import de.android.ayrathairullin.mariobros.MarioBros;
+import de.android.ayrathairullin.mariobros.screens.PlayScreen;
+
+public class Turtle extends Enemy {
+    public enum State {WALKING, SHELL}
+    public State currentState, previousState;
+    private float stateTime;
+    private Animation<TextureRegion> walkAnimation;
+    private Array<TextureRegion> frames;
+    private TextureRegion shell;
+    private boolean setToDestroy;
+    private boolean destroyed;
+
+    public Turtle(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+        frames = new Array<TextureRegion>();
+        frames.add(new TextureRegion(screen.getAtlas().findRegion("turtle"), 0, 0, 16, 24));
+        frames.add(new TextureRegion(screen.getAtlas().findRegion("turtle"), 16, 0, 16, 24));
+        shell = new TextureRegion(screen.getAtlas().findRegion("turtle"), 0, 0, 16, 24);
+        walkAnimation = new Animation<TextureRegion>(.2f, frames);
+        currentState = previousState = State.WALKING;
+        setBounds(getX(), getY(), 16 / MarioBros.PPM, 24 / MarioBros.PPM);
+    }
+
+    @Override
+    protected void defineEnemy() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(getX(), getY());
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(6 / MarioBros.PPM);
+        fdef.filter.categoryBits = MarioBros.ENEMY_BIT;
+        fdef.filter.maskBits = MarioBros.GROUND_BIT |
+                MarioBros.COIN_BIT |
+                MarioBros.BRICK_BIT |
+                MarioBros.ENEMY_BIT |
+                MarioBros.OBJECT_BIT |
+                MarioBros.MARIO_BIT;
+        fdef.shape = shape;
+        b2body.createFixture(fdef).setUserData(this);
+        // create the head here:
+        PolygonShape head = new PolygonShape();
+        Vector2[] vertice = new Vector2[4];
+        vertice[0] = new Vector2(- 5, 8).scl(1 / MarioBros.PPM);
+        vertice[1] = new Vector2(5, 8).scl(1 / MarioBros.PPM);
+        vertice[2] = new Vector2(- 3, 3).scl(1 / MarioBros.PPM);
+        vertice[3] = new Vector2(3, 3).scl(1 / MarioBros.PPM);
+        head.set(vertice);
+        fdef.shape = head;
+        fdef.restitution = .5f;
+        fdef.filter.categoryBits = MarioBros.ENEMY_HEAD_BIT;
+        b2body.createFixture(fdef).setUserData(this);
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void hitOnHead() {
+        if (currentState != State.SHELL) {
+
+        }
+    }
+}
